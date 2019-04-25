3c60c649ab6c70f10055d964f128752284cbae5a
advait-l <advait.lonkar@iiitb.org>
2019-04-25 11:57:27 +0530

    cpp code

--- GoldCoins.cpp
+++ GoldCoins.cpp
@@ -1,8 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int steps = 0;
+
+int token;
+
+int weightOfGenuineCoin(int, vector<int>);
+int compare(int, int);
+
 int main(){
-    vector<int> coins;
+    int n;
+    cin>>n;
+    vector<int> arr;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    token = arr[arr.size() - 1];
+
+    cout<<"Weight: "<< weightOfGenuineCoin(n, arr)<<endl;
+    cout<<"Steps: "<< steps << endl;
+    
+
+
     /*
     Comparison : If two coins are same - take them down as 1 together.
     */
@@ -22,4 +43,60 @@ int main(){
             if the coins vanish, then the last coin will be the gold coin
             
     */
+   return 0;
+}
+
+
+int weightOfGenuineCoin(int n, vector<int> arr){
+    vector<int> aux;
+    //Even
+    if(n%2 == 0){
+        int counter = 0;
+        while(counter < n-1){
+            if(compare(arr[counter], arr[counter + 1])){
+                aux.push_back(arr[counter]);
+            }
+            steps += 1;
+            counter += 2;
+        }
+        if(aux.size() == 1){
+            return aux[0];
+        } else if(aux.size() == 0){
+            return token;
+        } else{
+            return weightOfGenuineCoin(aux.size(), aux);
+        }
+
+    }
+
+    //Odd
+    else{
+        arr.erase(arr.end() - 1);
+        n = n - 1;
+        int counter = 0;
+        while(counter < n-1){
+            if(compare(arr[counter], arr[counter + 1])){
+                aux.push_back(arr[counter]);
+            }
+            steps += 1;
+            counter += 2;
+        }
+        if(aux.size() % 2 == 1){
+            token = aux[aux.size() - 1];
+        } 
+        if(aux.size() == 1){
+            return aux[0];
+        } else if(aux.size() == 0){
+            return token;
+        } else{
+            return weightOfGenuineCoin(aux.size(), aux);
+        }
+    }
+}
+
+int compare(int w1, int w2){
+    if(w1 == w2){
+        return 1;
+    }
+    return 0;
 }
