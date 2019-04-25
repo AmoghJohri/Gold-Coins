ab58f1431708e5382a9c6aa1cf02833d90563c73
Amogh Johri <32429407+AmoghJohri@users.noreply.github.com>
2019-04-25 19:57:03 +0530

    Create GoldCoinsModify.cpp
    Generates random test cases and gives the number of weighing possible, the number of 1s in the input and the number of 0s too.

---
+++ GoldCoinsModify.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Global variables declarations */
+int weighings = 0;
+int token;
+
+/* Functions declaration */
+int weightOfGenuineCoin(int, vector<int>);
+int balance(int, int);
+int B(int);
+
+int main(){
+    int numberOfCoins;
+    cin>>numberOfCoins;
+    vector<int> coins;
+    srand(time(0)); 
+
+    int numOf1s = 0;
+    int numOf0s = 0;
+
+    for(int i = 0; i < numberOfCoins; i++){
+        int x = rand()%2;
+        cout << x << " ";
+        if(x == 0)
+            numOf0s = numOf0s + 1;
+        else
+            numOf1s = numOf1s + 1;
+        coins.push_back(x);
+    }
+    token = coins[coins.size() - 1];    // Last coin
+    if(numOf0s == numOf1s)
+    {
+        cout << "\n" << "INVALID INPUT!!!" << "\n";
+        return 0;
+    }
+
+    cout<<"\n\nWeight of genuine coin: "<< weightOfGenuineCoin(numberOfCoins, coins)<<endl;
+    cout<<"Number of Weighings: "<< weighings << endl;
+    cout<<"Number of Weighing permitted: "<< numberOfCoins - B(numberOfCoins) << endl;
+    cout <<"Num Of 1s : " << numOf1s;
+    cout <<"\nNum Of 0s : " << numOf0s;
+    cout <<"\n";
+   return 0;
+}
+
+
+int weightOfGenuineCoin(int numberOfCoins, vector<int> coins){
+    vector<int> aux;
+    /* Case 1 : Number of coins are even */
+    if(numberOfCoins%2 == 0){
+        int counter = 0;
+        while(counter < numberOfCoins-1){
+            if(balance(coins[counter], coins[counter + 1])){
+                aux.push_back(coins[counter]);
+            }
+            weighings += 1;
+            counter += 2;
+        }
+        if(aux.size() == 1){
+            return aux[0];
+        } else if(aux.size() == 0){
+            return token;
+        } else{
+            return weightOfGenuineCoin(aux.size(), aux);
+        }
+
+    }
+
+    /* Case 2 : Number of coins are odd */
+    else{
+        coins.erase(coins.end() - 1);
+        numberOfCoins = numberOfCoins - 1;
+        int counter = 0;
+        while(counter < numberOfCoins-1){
+            if(balance(coins[counter], coins[counter + 1])){
+                aux.push_back(coins[counter]);
+            }
+            weighings += 1;
+            counter += 2;
+        }
+        if(aux.size() % 2 == 1){
+            token = aux[aux.size() - 1];
+        } 
+        if(aux.size() == 1){
+            return aux[0];
+        } else if(aux.size() == 0){
+            return token;
+        } else{
+            return weightOfGenuineCoin(aux.size(), aux);
+        }
+    }
+}
+
+/* Helper function */
+int balance(int w1, int w2){
+    if(w1 == w2){
+        return 1;
+    }
+    return 0;
+}
+
+int B(int num)
+{
+    int bin;
+    int output = 0;
+    while (num > 0)
+    {
+        bin = num % 2;
+        output = output + bin;
+        num /= 2;
+    }
+    return output;
+}
